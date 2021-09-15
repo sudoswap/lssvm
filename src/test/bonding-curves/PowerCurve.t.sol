6e0fe5916bec845c2d39bddb5f4a792f3445fa97
Owen <owenshen24@gmail.com>
2021-09-15 00:21:59 -0700

    small changes

--- src/test/bonding-curves/PowerCurve.t.sol
+++
@@ -1,9 +0,0 @@
-// SPDX-License-Identifier: AGPL-3.0
-pragma solidity ^0.8.0;
-
-import { DSTest } from "ds-test/test.sol";
-
-import { PowerCurve } from "../../bonding-curves/PowerCurve.sol";
-import { Hevm } from "../utils/Hevm.sol";
-
-contract PowerCurveTest is DSTest {}
