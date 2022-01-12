627208b96c742a8ce0bf9b599aebfdc5473fa385
ZeframLou <zeframlou@gmail.com>
2022-01-11 19:26:59 -0800

    perf: use new clone proxy that passes immutable pair params in calldata, remove deterministic pair deployment

--- src/lib/LSSVMPairCloner.sol
+++ src/lib/LSSVMPairCloner.sol
@@ -7,19 +7,14 @@ import {IERC721} from "@openzeppelin/contracts/token/ERC721/IERC721.sol";
 import {ICurve} from "../bonding-curves/ICurve.sol";
 import {LSSVMPairFactoryLike} from "../LSSVMPairFactoryLike.sol";
 
-/**
- * @dev https://eips.ethereum.org/EIPS/eip-1167[EIP 1167] is a standard for
- * deploying minimal proxy contracts, also known as "clones".
- *
- * LSSVMPairCloner slightly modifies EIP-1167 by appending immutable parameters
- * used by the deployed LSSVMPair to the minimal proxy bytecode, in order to
- * save gas on reading those parameters.
- */
 library LSSVMPairCloner {
     /**
      * @dev Deploys and returns the address of a clone that mimics the behaviour of `implementation`.
      *
      * This function uses the create opcode, which should never revert.
+     *
+     * During the delegate call, extra data is copied into the calldata which can then be
+     * accessed by the implementation contract.
      */
     function clone(
         address implementation,
@@ -30,114 +25,106 @@ library LSSVMPairCloner {
     ) internal returns (address instance) {
         assembly {
             let ptr := mload(0x40)
+
+            // -------------------------------------------------------------------------------------------------------------
+            // CREATION (11 bytes)
+            // -------------------------------------------------------------------------------------------------------------
+
+            // creation size = 0a
+            // runtime size = 73
+            // 3d          | RETURNDATASIZE        | 0                       | –
+            // 60 runtime  | PUSH1 runtime     (r) | r 0                     | –
+            // 80          | DUP1                  | r r 0                   | –
+            // 60 creation | PUSH1 creation (c)    | c r r 0                 | –
+            // 3d          | RETURNDATASIZE        | 0 c r r 0               | –
+            // 39          | CODECOPY              | r 0                     | [0-2d]: runtime code
+            // 81          | DUP2                  | 0 c  0                  | [0-2d]: runtime code
+            // f3          | RETURN                | 0                       | [0-2d]: runtime code
+
+            // -------------------------------------------------------------------------------------------------------------
+            // RUNTIME (54 bytes of code + 61 bytes of extra data = 115 bytes)
+            // -------------------------------------------------------------------------------------------------------------
+
+            // extra data size = 3d
+            // 36          | CALLDATASIZE          | cds                     | –
+            // 3d          | RETURNDATASIZE        | 0 cds                   | –
+            // 3d          | RETURNDATASIZE        | 0 0 cds                 | –
+            // 37          | CALLDATACOPY          | –                       | [0, cds] = calldata
+            // 60 extra    | PUSH1 extra           | extra                   | [0, cds] = calldata
+            // 60 0x36     | PUSH1 0x36            | 0x36 extra              | [0, cds] = calldata // 0x36 (54) is runtime size - data
+            // 36          | CALLDATASIZE          | cds 0x36 extra          | [0, cds] = calldata
+            // 39          | CODECOPY              | _                       | [0, cds] = calldata
+            // 3d          | RETURNDATASIZE        | 0                       | [0, cds] = calldata
+            // 3d          | RETURNDATASIZE        | 0 0                     | [0, cds] = calldata
+            // 3d          | RETURNDATASIZE        | 0 0 0                   | [0, cds] = calldata
+            // 36          | CALLDATASIZE          | cds 0 0 0               | [0, cds] = calldata
+            // 60 extra    | PUSH1 extra           | extra cds 0 0 0         | [0, cds] = calldata
+            // 01          | ADD                   | cds+extra 0 0 0         | [0, cds] = calldata
+            // 3d          | RETURNDATASIZE        | 0 cds 0 0 0             | [0, cds] = calldata
+            // 73 addr     | PUSH20 0x123…         | addr 0 cds 0 0 0        | [0, cds] = calldata
             mstore(
                 ptr,
-                0x3d606a80600a3d3981f3363d3d373d3d3d363d73000000000000000000000000
+                hex"3d_60_73_80_60_0a_3d_39_81_f3_36_3d_3d_37_60_3d_60_36_36_39_3d_3d_3d_36_60_3d_01_3d_73_00_00_00"
             )
-            mstore(add(ptr, 0x14), shl(0x60, implementation))
+            mstore(add(ptr, 0x1d), shl(0x60, implementation))
+
+            // 5a          | GAS                   | gas addr 0 cds 0 0 0    | [0, cds] = calldata
+            // f4          | DELEGATECALL          | success 0                | [0, cds] = calldata
+            // 3d          | RETURNDATASIZE        | rds success 0           | [0, cds] = calldata
+            // 82          | DUP3                  | 0 rds success 0         | [0, cds] = calldata
+            // 80          | DUP1                  | 0 0 rds success 0       | [0, cds] = calldata
+            // 3e          | RETURNDATACOPY        | success 0               | [0, rds] = return data (there might be some irrelevant leftovers in memory [rds, cds] when rds < cds)
+            // 90          | SWAP1                 | 0 success               | [0, rds] = return data
+            // 3d          | RETURNDATASIZE        | rds 0 success           | [0, rds] = return data
+            // 91          | SWAP2                 | success 0 rds           | [0, rds] = return data
+            // 60 0x36     | PUSH1 0x36            | 0x36 sucess 0 rds       | [0, rds] = return data
+            // 57          | JUMPI                 | 0 rds                   | [0, rds] = return data
+            // fd          | REVERT                | –                       | [0, rds] = return data
+            // 5b          | JUMPDEST              | 0 rds                   | [0, rds] = return data
+            // f3          | RETURN                | –                       | [0, rds] = return data
             mstore(
-                add(ptr, 0x28),
-                0x5af43d82803e903d91602b57fd5bf30000000000000000000000000000000000
+                add(ptr, 0x31),
+                0x5af43d82803e903d91603457fd5bf30000000000000000000000000000000000
             )
-            mstore(add(ptr, 0x37), shl(0x60, factory))
-            mstore(add(ptr, 0x4B), shl(0x60, bondingCurve))
-            mstore(add(ptr, 0x5F), shl(0x60, nft))
-            mstore8(add(ptr, 0x73), poolType)
-            instance := create(0, ptr, 0x74)
+
+            // -------------------------------------------------------------------------------------------------------------
+            // EXTRA DATA (61 bytes)
+            // -------------------------------------------------------------------------------------------------------------
+
+            mstore(add(ptr, 0x40), shl(0x60, factory))
+            mstore(add(ptr, 0x54), shl(0x60, bondingCurve))
+            mstore(add(ptr, 0x68), shl(0x60, nft))
+            mstore8(add(ptr, 0x7c), poolType)
+
+            instance := create(0, ptr, 0x7d)
         }
-        require(instance != address(0), "ERC1167: create failed");
     }
 
-    /**
-     * @dev Deploys and returns the address of a clone that mimics the behaviour of `implementation`.
-     *
-     * This function uses the create2 opcode and a `salt` to deterministically deploy
-     * the clone. Using the same `implementation` and `salt` multiple time will revert, since
-     * the clones cannot be deployed twice at the same address.
-     */
-    function cloneDeterministic(
-        address implementation,
-        LSSVMPairFactoryLike factory,
-        ICurve bondingCurve,
-        IERC721 nft,
-        uint8 poolType,
-        bytes32 salt
-    ) internal returns (address instance) {
+    function isClone(address implementation, address query)
+        internal
+        view
+        returns (bool result)
+    {
+        // solhint-disable-next-line no-inline-assembly
         assembly {
             let ptr := mload(0x40)
             mstore(
                 ptr,
-                0x3d606a80600a3d3981f3363d3d373d3d3d363d73000000000000000000000000
+                hex"36_3d_3d_37_60_73_60_36_36_39_3d_3d_3d_36_60_73_01_3d_73_00_00_00_00_00_00_00_00_00_00_00_00_00"
             )
-            mstore(add(ptr, 0x14), shl(0x60, implementation))
+            mstore(add(ptr, 0x13), shl(0x60, implementation))
             mstore(
-                add(ptr, 0x28),
-                0x5af43d82803e903d91602b57fd5bf30000000000000000000000000000000000
+                add(ptr, 0x27),
+                0x5af43d82803e903d91603457fd5bf30000000000000000000000000000000000
             )
-            mstore(add(ptr, 0x37), shl(0x60, factory))
-            mstore(add(ptr, 0x4B), shl(0x60, bondingCurve))
-            mstore(add(ptr, 0x5F), shl(0x60, nft))
-            mstore8(add(ptr, 0x73), poolType)
-            instance := create2(0, ptr, 0x74, salt)
-        }
-        require(instance != address(0), "ERC1167: create2 failed");
-    }
 
-    /**
-     * @dev Computes the address of a clone deployed using {Clones-cloneDeterministic}.
-     */
-    function predictDeterministicAddress(
-        address implementation,
-        LSSVMPairFactoryLike factory,
-        ICurve bondingCurve,
-        IERC721 nft,
-        uint8 poolType,
-        bytes32 salt,
-        address deployer
-    ) internal pure returns (address predicted) {
-        assembly {
-            let ptr := mload(0x40)
-            mstore(
-                ptr,
-                0x3d606a80600a3d3981f3363d3d373d3d3d363d73000000000000000000000000
+            // compare expected bytecode with that of the queried contract
+            let other := add(ptr, 0x40)
+            extcodecopy(query, other, 0, 0x36)
+            result := and(
+                eq(mload(ptr), mload(other)),
+                eq(mload(add(ptr, 0x16)), mload(add(other, 0x16)))
             )
-            mstore(add(ptr, 0x14), shl(0x60, implementation))
-            mstore(
-                add(ptr, 0x28),
-                0x5af43d82803e903d91602b57fd5bf30000000000000000000000000000000000
-            )
-            mstore(add(ptr, 0x37), shl(0x60, factory))
-            mstore(add(ptr, 0x4b), shl(0x60, bondingCurve))
-            mstore(add(ptr, 0x5f), shl(0x60, nft))
-            mstore8(add(ptr, 0x73), poolType)
-            mstore8(add(ptr, 0x74), 0xff)
-            mstore(add(ptr, 0x75), shl(0x60, deployer))
-            mstore(add(ptr, 0x89), salt)
-            mstore(add(ptr, 0xa9), keccak256(ptr, 0x74))
-            predicted := keccak256(add(ptr, 0x74), 0x55)
         }
-    }
-
-    /**
-     * @dev Computes the address of a clone deployed using {Clones-cloneDeterministic}.
-     */
-    function predictDeterministicAddress(
-        address implementation,
-        LSSVMPairFactoryLike factory,
-        ICurve bondingCurve,
-        IERC721 nft,
-        uint8 poolType,
-        bytes32 salt
-    ) internal view returns (address predicted) {
-        return
-            predictDeterministicAddress(
-                implementation,
-                factory,
-                bondingCurve,
-                nft,
-                poolType,
-                salt,
-                address(this)
-            );
     }
 }
