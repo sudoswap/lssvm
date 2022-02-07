edfd4a93acc1c9b4d2a1438af6d8e667ff901971
Owen <sudo@example.com>
2022-02-07 00:25:48 -0800

    Comment explaining why we use solmate

--- src/LSSVMPairFactory.sol
+++ src/LSSVMPairFactory.sol
@@ -6,6 +6,8 @@ import {IERC721} from "@openzeppelin/contracts/token/ERC721/IERC721.sol";
 import {ERC165Checker} from "@openzeppelin/contracts/utils/introspection/ERC165Checker.sol";
 import {IERC721Enumerable} from "@openzeppelin/contracts/token/ERC721/extensions/IERC721Enumerable.sol";
 
+// @dev Solmate's ERC20 is used instead of OZ's ERC20 so we can use safeTransferLib for cheaper safeTransfers for 
+// ETH and ERC20 tokens
 import {ERC20} from "solmate/tokens/ERC20.sol";
 import {SafeTransferLib} from "solmate/utils/SafeTransferLib.sol";
 
