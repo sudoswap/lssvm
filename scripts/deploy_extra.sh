2338368caa95f2978582278e80253b74e2d49cf6
Bishal Bist <bisb@pop-os.localdomain>
2022-11-10 10:57:44 +0545

    fix incorrect env var name

--- scripts/deploy_extra.sh
+++ scripts/deploy_extra.sh
@@ -9,7 +9,6 @@ export ADDRESSES_FILE="$OUT_DIR/addresses_extra.json"
 # import the deployment helpers
 . $(dirname $0)/common.sh
 
-export LSSVMRouterAddr="${LSSVMRouterAddr:-$(jq -r .LSSVMRouter $CONTRACTS_FILE)}"
 export LSSVMPairFactoryAddr="${LSSVMPairFactoryAddr:-$(jq -r .LSSVMPairFactory $CONTRACTS_FILE)}"
 
 # Deploy tokens
@@ -22,13 +21,13 @@ send $Test20Addr "mint(address,uint256)" $ETH_FROM 1000000000000000000000000 # 1
 log "100M T20 ($Test20Addr) minted for $ETH_FROM"
 
 # Test721BatchMintWithRoyalty
-Test721Addr=$(deploy Test721BatchMintWithRoyalty ${ROYALTY_RECIPIENT:-$ETH_fROM} 0 100000000000000) # flat, 0.0001 ETH
+Test721Addr=$(deploy Test721BatchMintWithRoyalty ${ROYALTY_RECIPIENT:-$ETH_FROM} 0 100000000000000) # flat, 0.0001 ETH
 log "Test721 deployed at:" $Test721Addr
 
 send $Test721Addr "batchMint(address,uint256[])" $ETH_FROM "[$(seq -s ',' 0 99)]"
 
 # Test721EnumerableBatchMintWithRoyalty
-Test721EnumerableAddr=$(deploy Test721EnumerableBatchMintWithRoyalty ${ROYALTY_RECIPIENT:-$ETH_fROM} 1 10000000000000000) # percent, 0.01 * salePrice ETH
+Test721EnumerableAddr=$(deploy Test721EnumerableBatchMintWithRoyalty ${ROYALTY_RECIPIENT:-$ETH_FROM} 1 10000000000000000) # percent, 0.01 * salePrice ETH
 log "Test721Enumerable deployed at:" $Test721EnumerableAddr
 
 send $Test721EnumerableAddr "batchMint(address,uint256[])" $ETH_FROM "[$(seq -s ',' 0 99)]"
