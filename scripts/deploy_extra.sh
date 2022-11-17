b1901fc4d4343d8731120f5fe9520d5863815443
bbist <pro.bbist@gmail.com>
2022-11-17 12:22:07 +0545

    make royalty address configurable

--- scripts/deploy_extra.sh
+++ scripts/deploy_extra.sh
@@ -9,6 +9,7 @@ export ADDRESSES_FILE="$OUT_DIR/addresses_extra.json"
 # import the deployment helpers
 . $(dirname $0)/common.sh
 
+export LSSVMRouterAddr="${LSSVMRouterAddr:-$(jq -r .LSSVMRouter $CONTRACTS_FILE)}"
 export LSSVMPairFactoryAddr="${LSSVMPairFactoryAddr:-$(jq -r .LSSVMPairFactory $CONTRACTS_FILE)}"
 
 # Deploy tokens
@@ -21,13 +22,13 @@ send $Test20Addr "mint(address,uint256)" $ETH_FROM 1000000000000000000000000 # 1
 log "100M T20 ($Test20Addr) minted for $ETH_FROM"
 
 # Test721BatchMintWithRoyalty
-Test721Addr=$(deploy Test721BatchMintWithRoyalty ${ROYALTY_RECIPIENT:-$ETH_FROM} 0 100000000000000) # flat, 0.0001 ETH
+Test721Addr=$(deploy Test721BatchMintWithRoyalty ${ROYALTY_RECIPIENT:-$ETH_fROM} 0 100000000000000) # flat, 0.0001 ETH
 log "Test721 deployed at:" $Test721Addr
 
 send $Test721Addr "batchMint(address,uint256[])" $ETH_FROM "[$(seq -s ',' 0 99)]"
 
 # Test721EnumerableBatchMintWithRoyalty
-Test721EnumerableAddr=$(deploy Test721EnumerableBatchMintWithRoyalty ${ROYALTY_RECIPIENT:-$ETH_FROM} 1 10000000000000000) # percent, 0.01 * salePrice ETH
+Test721EnumerableAddr=$(deploy Test721EnumerableBatchMintWithRoyalty ${ROYALTY_RECIPIENT:-$ETH_fROM} 1 10000000000000000) # percent, 0.01 * salePrice ETH
 log "Test721Enumerable deployed at:" $Test721EnumerableAddr
 
 send $Test721EnumerableAddr "batchMint(address,uint256[])" $ETH_FROM "[$(seq -s ',' 0 99)]"
@@ -36,6 +37,15 @@ send $Test721EnumerableAddr "batchMint(address,uint256[])" $ETH_FROM "[$(seq -s 
 LSSVMRouterWithRoyaltiesAddr=$(deploy LSSVMRouterWithRoyalties $LSSVMPairFactoryAddr)
 log "LSSVMRouterWithRoyalties deployed at:" $LSSVMRouterWithRoyaltiesAddr
 
+# Initialize royaltyRegistry address
+send $LSSVMRouterWithRoyaltiesAddr "initializeRoyaltyRegistry(address)" ${ROYALTY_REGISTRY:-0x0000000000000000000000000000000000000000}
+ROYALTY_REGISTRY="0x$(seth --rpc-url $ETH_RPC_URL call $LSSVMRouterWithRoyaltiesAddr "ROYALTY_REGISTRY()" | tail -c 41)"
+log "LSSVMRoyaltyWithRoyalties initialized with address $ROYALTY_REGISTRY"
+
+# save royalty registry
+saveContract RoyaltyRegistry "$ROYALTY_REGISTRY"
+
 # Whitelist routerWithRoyalties in factory
 send $LSSVMPairFactoryAddr "setRouterAllowed(address,bool)" $LSSVMRouterWithRoyaltiesAddr true
 log "Whitelisted routerWithRoyalties in factory"
+
